3d9d7206ec55f3ad86fee1b850962430b62d60a7
NrKxFDFb <kozyakvit@gmail.com>
2021-04-22 14:47:53 +0300

    created database for logging

--- website/auth.py
+++ website/auth.py
@@ -1,16 +1,33 @@
-from flask import Blueprint, render_template, request,flash
-
+from flask import Blueprint, render_template, request,flash,redirect,url_for
+from .models import User
+from werkzeug.security import generate_password_hash,check_password_hash
+from . import db
+from flask_login import login_user,login_required,logout_user,current_user
 auth = Blueprint('auth',__name__)
 
 @auth.route('/login',methods = ['GET','POST'])
-def lodin():
-
-    return render_template("login.html")
+def login():
+    if request.method == "POST":
+        email = request.form.get('email')
+        password = request.form.get('password')
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password,password):
+                flash("Logged in succesfully!",category='success')
+                login_user(user,remember=True)
+                return redirect(url_for("views.home"))
+            else:
+                flash("Incorrect password, try again!",category='error')
+        else:
+            flash("Email does not exist!",category='error')
+    return render_template("login.html",user=current_user)
 
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "<p>logout</p>"
+    logout_user()
+    return redirect(url_for('auth.login'))
 @auth.route('/sign-up',methods = ['GET','POST'])
 def sign_up():
     if request.method == 'POST':
@@ -18,7 +35,10 @@ def sign_up():
         first_name = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
-        if len(email) < 6:
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash("Email already exist!",category='error')
+        elif len(email) < 6:
             flash('Email length must be more than 5 characters! ',category='error')
         elif len(first_name) < 3 :
             flash('Name length must be more than 3 characters! ',category='error')
@@ -27,6 +47,12 @@ def sign_up():
         elif password1 != password2:
             flash('Passwords do not match',category='error')
         else:
-            flash('Account created!',category='success')
+            new_user = User(email=email,first_name=first_name,password=generate_password_hash(password1,method="sha256"))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(user, remember=True)
 
-    return render_template('sign_up.html')
+            flash('Account created!',category='success')
+            return redirect(url_for("views.home"))
+
+    return render_template('sign_up.html',user=current_user)
