ce2a4bff56bcb8b8152b992fbee4bb9eaf2e69ab
NrKxFDFb <kozyakvit@gmail.com>
2021-04-22 15:13:08 +0300

    deeleted unnecessary class

--- website/models.py
+++ website/models.py
@@ -2,10 +2,7 @@ from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
-class Note(db.Model):
-    id = db.Column(db.Integer,primary_key=True)
-    date = db.Column(db.String(10000))
-    date = db.Column(db.DateTime(timezone=True),default=func.now())
+
 
 
 class User(db.Model,UserMixin):
