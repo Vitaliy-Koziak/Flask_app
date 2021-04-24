74bd648b0b3c2686c51e196a893f63e2e564ef23
NrKxFDFb <kozyakvit@gmail.com>
2021-04-24 11:51:25 +0300

    small changes in front-end

--- website/__init__.py
+++ website/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
+
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
@@ -10,7 +11,6 @@ def create_app():
     app.config['SECRET_KEY'] = 'vitaliy'
     app.config['SQLALCHEMY_DATABASE_URI']=f'sqlite:///{DB_NAME}'
     db.init_app(app)
-
 
 
     from .views import views
@@ -24,10 +24,13 @@ def create_app():
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
 
+
     @login_manager.user_loader
     def load_user(id):
         return User.query.get(int(id))
     return app
+
+
 
 def create_database(app):
     if not path.exists('website/'+DB_NAME):
