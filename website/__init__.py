fa5e542ac2f50027cad3952d285ca89b5be16ab3
NrKxFDFb <kozyakvit@gmail.com>
2021-05-01 14:57:22 +0300

    show all amployees

--- website/__init__.py
+++ website/__init__.py
@@ -1,16 +1,20 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
+from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
+from flask_restful import Resource, Api
 
 db = SQLAlchemy()
-DB_NAME = "database.db"
+DB_NAME = "database2.db"
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'vitaliy'
-    app.config['SQLALCHEMY_DATABASE_URI']=f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
+    api = Api(app)
+
 
 
     from .views import views
@@ -18,11 +22,14 @@ def create_app():
     app.register_blueprint(views,url_prefix = '/')
     app.register_blueprint(auth,url_prefix = '/')
     from .models import User
-
     create_database(app)
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
+
+
+    # from website.rest import UsersResourseList
+    # api.add_resource(UsersResourseList,"/users")
 
 
     @login_manager.user_loader
@@ -35,5 +42,5 @@ def create_app():
 def create_database(app):
     if not path.exists('website/'+DB_NAME):
         db.create_all(app=app)
-        print('Created DB!')
+
 
--- website/models.py
+++ website/models.py
@@ -1,6 +1,10 @@
-from . import db
+from flask import Flask
+
+from .import db
 from flask_login import UserMixin
-from sqlalchemy.sql import func
+from flask_marshmallow import Marshmallow
+app = Flask(__name__)
+ma = Marshmallow(app)
 
 
 class User(db.Model,UserMixin):
@@ -8,3 +12,19 @@ class User(db.Model,UserMixin):
     email = db.Column(db.String(40),unique=True)
     password = db.Column(db.String(30))
     first_name = db.Column(db.String(150))
+    date = db.Column(db.String(20))
+    department = db.Column(db.String(50))
+    salary = db.Column(db.Integer)
+
+class UserSchema(ma.Schema):
+    class Meta:
+        # Fields to expose
+        fields = ("email", "first_name","date","department","salary")
+
+    # Smart hyperlinking
+    _links = ma.Hyperlinks(
+        {
+            "self": ma.URLFor("user_detail", values=dict(id="<id>")),
+            "collection": ma.URLFor("users"),
+        }
+    )
